3bc3a63b0a93d983bf9f2b0975f016d502a8793d
nagesha technology <asbconstruction19@gmail.com>
2024-03-16 23:31:18 +0530

    create expense project

---
+++ expense-v1/ec2.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [sg-0cd62542b986233c8]
+  tags = {
+    Name = frontend
+  }
+}
+
+
+resource "aws_instance" "backend" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [sg-0cd62542b986233c8]
+  tags = {
+    Name = backend
+  }
+}
+
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [sg-0cd62542b986233c8]
+  tags = {
+    Name = mysql
+  }
+}
