5b71ad45c4802d0dc8edcb059e3709d1b50436c5
nagesha technology <asbconstruction19@gmail.com>
2024-03-17 19:21:10 +0530

    double quotes

--- expense-v1/ec2.tf
+++ expense-v1/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-0cd62542b986233c8]
+  vpc_security_group_ids = ["sg-0cd62542b986233c8"]
   tags = {
     Name = "frontend"
   }
@@ -11,7 +11,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "backend" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-0cd62542b986233c8]
+  vpc_security_group_ids = ["sg-0cd62542b986233c8"]
   tags = {
     Name = "backend"
   }
@@ -21,7 +21,7 @@ resource "aws_instance" "backend" {
 resource "aws_instance" "mysql" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-0cd62542b986233c8]
+  vpc_security_group_ids = ["sg-0cd62542b986233c8"]
   tags = {
     Name = "mysql"
   }
