a62cb4d17e4a2c0a3b4622e13298816d71d12311
nagesha technology <asbconstruction19@gmail.com>
2024-03-24 22:50:35 +0530

    provisioner sudo removed

---
+++ provisioner/main.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "test" {
+  ami                    = "ami-05f020f5935e52dc4"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+}
+
+resource "null_resource" "provisioner" {
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.test.public_ip
+    }
+
+    inline = [
+      " dnf install nginx",
+      "sudo dnf enable nginx}",
+    ]
+  }
+}
+
+data "aws_security_group" "selected" {
+  name = "allow-all"
+}
+
+
